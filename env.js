7295a25c058bf6d0fb4993b0bce6916bc7e41463
roshanxbot <110165893+roshanxbot@users.noreply.github.com>
2025-07-11 10:16:56 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-const token = "7040611799:AAF9TBAXwm9Pp1wk9aa7TOd3dVaxgQGTVDc";
+const token = "8173403758:AAHhgYtJ7q3mcQ1RtuzuKa4xETSoqsN8LaA";
 
 module.exports = {
   ADMIN: process.env.ADMIN || "1260610452, 6332950297, 5535023701, 5691953694, 6053122111",
